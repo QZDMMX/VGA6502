b3c80aac04757f0ecbde2830edb97bbb4f884de7
QZDMMX <ledpower@163.com>
2018-05-04 18:02:05 +0800

    初步VGA发生器，0和639列显示蓝色
    Signed-off-by: QZDMMX <ledpower@163.com>

--- VGA_Mixer.vhd
+++ VGA_Mixer.vhd
@@ -1,4 +1,4 @@
---qv8
+--
 --
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
@@ -10,13 +10,22 @@ USE lpm.lpm_components.ALL;
 
 ENTITY VGA_Mixer IS
 
-   PORT(SIGNAL Clock 					: IN std_logic;
-        SIGNAL RB1,GB1,BB1 				: IN std_logic;
-        SIGNAL RB2,GB2,BB2				: IN std_logic;
-        SIGNAL RBOX,GBOX,BBOX			: IN std_logic;
-        SIGNAL RTXT,GTXT,BTXT			: IN std_logic;
+   PORT(SIGNAL Clock 							: IN std_logic;
+        SIGNAL H_RESET,H_ON,V_ON	: IN std_logic;
+        SIGNAL H_POS,V_POS 				: IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+				SIGNAL SPI_CE,SPI_CK,SPI_DI: IN std_logic;
+				
+        SIGNAL GRAM_DAT 					: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        SIGNAL FONT_DAT						: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        
+        SIGNAL VRAM_ADDR					: OUT STD_LOGIC_VECTOR(16 DOWNTO 0); 
+        SIGNAL VRAM_OE,VRAM_WE,VRAM_CE: OUT std_logic;
+        
+        SIGNAL RGB_OUT						: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
 		
-		SIGNAL RO,GO,BO                 : OUT std_logic );		
+        SIGNAL ST_OUT							: OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+		    SIGNAL T1,T2,T3                 : OUT std_logic );		
 END VGA_Mixer;
 
 architecture behavior of VGA_Mixer is
@@ -25,9 +34,7 @@ BEGIN
 VGA_RGB: process(Clock)
 BEGIN 
 	IF Clock'event and Clock='1' THEN
-		RO<=RB1 OR RB2 OR RBOX OR RTXT;
-		GO<=GB1 OR GB2 OR GBOX OR GTXT;
-		BO<=BB1 OR BB2 OR BBOX OR BTXT;
+
 	END IF;
 END process VGA_RGB;	
 
--- VGA_SYNC.vhd
+++ VGA_SYNC.vhd
@@ -1,24 +1,35 @@
-%VGA Generator%
+
 library IEEE;
 use  IEEE.STD_LOGIC_1164.all;
 use  IEEE.STD_LOGIC_ARITH.all;
 use  IEEE.STD_LOGIC_UNSIGNED.all;
 
 ENTITY VGA_SYNC IS
-	PORT(	clock_25Mhz, red, green, blue		: IN	STD_LOGIC;
-			red_out, green_out, blue_out, horiz_sync_out, vert_sync_out	: OUT	STD_LOGIC;
-			pixel_row, pixel_column: OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
+  PORT( clock_25Mhz                     : IN  STD_LOGIC; 
+        red, green                      : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        blue                            : IN STD_LOGIC_VECTOR(1 DOWNTO 0);    
+        red_out, green_out              : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+        blue_out                        : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+        horiz_sync_out, vert_sync_out   : OUT STD_LOGIC;
+        h_reset_out,v_on_h,v_on_v 		: OUT STD_LOGIC;
+        pixel_row, pixel_column         : OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
 END VGA_SYNC;
 
 ARCHITECTURE a OF VGA_SYNC IS
-	SIGNAL horiz_sync, vert_sync : STD_LOGIC;
-	SIGNAL video_on, video_on_v, video_on_h,video_on2 : STD_LOGIC;
-	SIGNAL h_count, v_count :STD_LOGIC_VECTOR(9 DOWNTO 0);
-
+  SIGNAL horiz_sync, vert_sync,h_reset : STD_LOGIC;
+  SIGNAL video_on, video_on_v, video_on_h : STD_LOGIC;
+  SIGNAL h_count, v_count :STD_LOGIC_VECTOR(9 DOWNTO 0);
+  SIGNAL Board_B:STD_LOGIC_VECTOR(1 DOWNTO 0);
 BEGIN
 
 -- video_on is high only when RGB data is displayed
-video_on <= video_on_H AND video_on_V;
+video_on <= video_on_h AND video_on_v;
+v_on_h<=video_on_h;
+v_on_v<=video_on_v;
+
+h_reset_out<=h_reset;
+pixel_row <= v_count;
+pixel_column <= h_count;
 
 -- 
 --  Horiz_sync  ------------------------------------__________--------
@@ -30,65 +41,74 @@ video_on <= video_on_H AND video_on_V;
 
 PROCESS
 BEGIN
-	WAIT UNTIL(clock_25Mhz'EVENT) AND (clock_25Mhz='1');
-		video_on2<=video_on;
-		
+  WAIT UNTIL(clock_25Mhz'EVENT) AND (clock_25Mhz='1');
+    
 --Generate Horizontal and Vertical Timing Signals for Video Signal
 -- H_count counts pixels (640 + extra time for sync signals)
-
-	IF (h_count = 799) THEN
-   		h_count <= "0000000000";
-	ELSE
-   		h_count <= h_count + 1;
-	END IF;
+  IF (h_count = 798) THEN
+      h_reset<='1';
+  ELSE
+      h_reset<='0';
+  END IF;
+  
+  IF (h_reset = '1') THEN
+      h_count <= "0000000000";
+  ELSE
+      h_count <= h_count + 1;
+  END IF;
 
 --Generate Horizontal Sync Signal using H_count
-	IF (h_count <= 755) AND (h_count >= 659) THEN
- 	  	horiz_sync <= '0';
-	ELSE
- 	  	horiz_sync <= '1';
-	END IF;
+  IF (h_count <= 755) AND (h_count >= 659) THEN
+      horiz_sync <= '0';
+  ELSE
+      horiz_sync <= '1';
+  END IF;
 
 --V_count counts rows of pixels (480 + extra time for sync signals)
 --  
 --  Vert_sync      -----------------------------------------------_______------------
 --  V_count         0                                      480    493-494          524
 --
-	IF (v_count >= 524) AND (h_count >= 699) THEN
-   		v_count <= "0000000000";
-	ELSIF (h_count = 699) THEN
-   		v_count <= v_count + 1;
-	END IF;
+  IF (v_count >= 524) AND (h_count >= 699) THEN
+      v_count <= "0000000000";
+  ELSIF (h_count = 699) THEN
+      v_count <= v_count + 1;
+  END IF;
 
 -- Generate Vertical Sync Signal using V_count
-	IF (v_count <= 494) AND (v_count >= 493) THEN
-   		vert_sync <= '0';
-	ELSE
-  		vert_sync <= '1';
-	END IF;
+  IF (v_count <= 494) AND (v_count >= 493) THEN
+      vert_sync <= '0';
+  ELSE
+      vert_sync <= '1';
+  END IF;
 
 -- Generate Video on Screen Signals for Pixel Data
-	IF (h_count <= 639) THEN
-   		video_on_h <= '1';
-   		pixel_column <= h_count;
-	ELSE
-	   	video_on_h <= '0';
-	END IF;
+  IF (h_count <= 639) THEN
+      video_on_h <= '1';
+--      pixel_column <= h_count;
+  ELSE
+      video_on_h <= '0';
+  END IF;
 
-	IF (v_count <= 479) THEN
-   		video_on_v <= '1';
-   		pixel_row <= v_count;
-	ELSE
-   		video_on_v <= '0';
-	END IF;
+  IF (v_count <= 479) THEN
+      video_on_v <= '1';
+--      pixel_row <= v_count;
+  ELSE
+      video_on_v <= '0';
+  END IF;
 
-	
+  
 -- Put all video signals through DFFs to elminate any delays that cause a blurry image
-		red_out <= red AND video_on2;
-		green_out <= green AND video_on2;
-		blue_out <= blue AND video_on2;
-		horiz_sync_out <= horiz_sync;
-		vert_sync_out <= vert_sync;
+    red_out <= red AND video_on & video_on & video_on;
+    green_out <= green AND video_on & video_on & video_on;
+    IF (h_count=0) OR (h_count=639) THEN
+		Board_B <="11";
+	ELSE
+		Board_B <="00";
+	END IF;
+	blue_out <= (blue or Board_B) AND video_on & video_on;
+    horiz_sync_out <= horiz_sync;
+    vert_sync_out <= vert_sync;
 
 END PROCESS;
 END a;
